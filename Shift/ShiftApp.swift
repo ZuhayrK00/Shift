53e7205afad4b029e137dfe8e5dfe4b102140f24
ZuhayrK00 <zuhayrk00@gmail.com>
2026-04-13 09:29:51 +0100

    Initial Commit

---
+++ Shift/ShiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShiftApp.swift
+//  Shift
+//
+//  Created by Zuhayr Khan on 13/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct ShiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
